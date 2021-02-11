b60a0de76c6b8718549500bab2eb71f25c11a37b
tharun.karam <csetarun2@gmail.com>
2021-02-11 09:29:53 +0530

    Create vars.tf

---
+++ terraform-deploy-webserver/vars.tf
@@ -0,0 +1,11 @@
+
+variable "access_key" {
+  description = " SSH access."
+  type        = string
+}
+
+
+variable "secret_key" {
+  description = "EC2 Instance for SSH access."
+  type        = string
+}
